2ab5a7154158800a048996d1d078322438865282
Icelle <icelleku@gmail.com>
2013-12-12 08:41:31 -0500

    Rails setup, set up postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MassHealth::Application.config.secret_key_base = '66ff1a9e92c052f41ff27fe330fa122f3022f3af49e5c9d4564c992874a94b594d0ccfc1dc3c541de22286d2e17e42b3e702df6b7b1200f4b5010b60c5a6d810'
